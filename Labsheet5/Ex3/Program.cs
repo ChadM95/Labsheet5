7451ce60261d11c355d99899f604713820ab074a
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-11-04 23:15:13 +0000

    use methods in program

--- Labsheet5/Ex3/Program.cs
+++ Labsheet5/Ex3/Program.cs
@@ -4,7 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Rectangle R1 = new Rectangle(50, 25);
+        Rectangle R2 = new Rectangle(50, 60);
+
+        int r1Perimeter = R1.CalcPerimeter();
+        int r2Perimeter = R2.CalcPerimeter();
+
+        int r1Area = R1.CalcArea();
+        int r2Area = R2.CalcArea();
+
+        if (r1Perimeter > r2Perimeter)
+        {
+            Console.WriteLine($"Area of Rectangle 1 is {r1Area}");
+        }
+
+        else if (r1Perimeter < r1Perimeter)
+        {
+            Console.WriteLine($"Area of Rectangle 2 is {r2Area}");
+        }
+
+
+        Console.ReadLine();
     }
 }
 
