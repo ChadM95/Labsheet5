47108fbdf0ae81cae0757a06653e22c4c1a9269c
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-11-04 23:07:32 +0000

    add CalcArea and CalcPerimeter methods

--- Labsheet5/Ex3/Rectangle.cs
+++ Labsheet5/Ex3/Rectangle.cs
@@ -3,9 +3,25 @@ namespace Ex3
 {
 	public class Rectangle
 	{
-		public Rectangle()
+		public int Length { get; set; }
+		public int Width { get; set; }
+
+		public Rectangle(int length, int width)
 		{
+			Length = length;
+			Width = width;
 		}
+
+		public int CalcPerimeter()
+		{
+			return (Length * 2) + (Width * 2);
+		}
+
+		public int CalcArea()
+		{
+			return Length * Width;
+		}
+
 	}
 }
 
