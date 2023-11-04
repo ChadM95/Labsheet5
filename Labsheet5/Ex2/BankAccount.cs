ac27cc0110ba7ef6a1fbca45820b088a777cf654
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-11-04 22:37:34 +0000

    add BankAccount class properties and constructor

---
+++ Labsheet5/Ex2/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Ex2
+{
+	public class BankAccount
+	{
+		//properties
+		public string AccNo { get; set; }
+        public int Balance { get; set; }
+        public int OverdraftLimit { get; set; }
+
+
+
+		//constructor 1
+        public BankAccount(string accNo, int overdraftLimit)
+		{
+			AccNo = accNo;
+			OverdraftLimit = overdraftLimit;
+		}
+
+
+
+	}
+}
+
