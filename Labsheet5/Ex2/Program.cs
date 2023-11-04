0544228e8ea725d4a9a8c7f35dcc3265141f39db
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-11-04 22:43:53 +0000

    add 2 objects and use PrintDetails method

--- Labsheet5/Ex2/Program.cs
+++ Labsheet5/Ex2/Program.cs
@@ -4,7 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        BankAccount B1 = new BankAccount("B123", 5000);
+
+        BankAccount B2 = new BankAccount("B456",5000);
+
+        B1.PrintDetails();
+
+        B2.PrintDetails();
+
+        Console.ReadLine();
     }
 }
 
