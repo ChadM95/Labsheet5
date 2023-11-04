25b2894fe981186b722bfd4db348037c3067451b
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-11-04 22:11:52 +0000

    use tostring method on new class

--- Labsheet5/Labsheet5/Ex1.cs
+++ Labsheet5/Labsheet5/Ex1.cs
@@ -4,7 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Student s1 = new Student("Chad","Computing",10); 
+
+        Console.WriteLine(s1);
+
+        Console.ReadLine();
     }
 }
 
--- Labsheet5/Labsheet5/Student.cs
+++ Labsheet5/Labsheet5/Student.cs
@@ -3,11 +3,12 @@ namespace Labsheet5
 {
 	public class Student
 	{
+		//properties
 		public string Name { get; set; }
 		public string Course { get; set; }
 		public int GPA { get; set; }
 
-
+		//constructor
 		public Student(string name, string course, int gpa)
 		{
 			Name = name;
@@ -15,10 +16,17 @@ namespace Labsheet5
 			GPA = gpa;
 		}
 
+		//ToString method override
         public override string ToString()
         {
 			return $"{Name} {Course} {GPA}";
         }
+
+		//Regular Method
+		public static int AdjustGPA(int percentage)
+		{
+			return GPA (percentage / 100); 
+		}
     }
 }
 
