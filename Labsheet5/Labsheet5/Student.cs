e235413f6c0dcc72bd52cbb07236ff3cfab17e93
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-11-04 21:56:44 +0000

    Ex1 add student class and properties

---
+++ Labsheet5/Labsheet5/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Labsheet5
+{
+	public class Student
+	{
+		public string Name { get; set; }
+		public string Course { get; set; }
+		public int GPA { get; set; }
+
+
+		public Student(string name, string course, int gpa)
+		{
+			Name = name;
+			Course = course;
+			GPA = gpa;
+		}
+	}
+}
+
